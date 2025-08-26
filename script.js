245e1b5128002419bfa84e0367630c4e78592612
ROGIMÉRIO RIBEIRO <87278772+Rogimerio@users.noreply.github.com>
2025-08-26 06:34:04 -0300

    Add index
    Meu Site RBB

---
+++ script.js
@@ -0,0 +1,6 @@
+// Menu responsivo
+const toggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+toggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
